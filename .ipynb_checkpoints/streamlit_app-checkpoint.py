ba4fbcca67112b518c8530e19256755f2bb3f9c4
yanejw <yanejw@gmail.com>
2023-09-28 12:45:59 +0800

    amendments to streamlit_app

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
-import sklearn
 
 # Title of the webpage
 st.title('Sustainability Classification for Fashion Products')
